1e5ba1934f6a82cc869aaa9835f23f0361872cb3
JariPC <jari,bloem@live,nl>
2017-07-03 12:23:13 +0200

    Comment met mysql connectie info toegevoegd

--- src/main/java/database/jdbc/Mysql.java
+++ src/main/java/database/jdbc/Mysql.java
@@ -7,6 +7,16 @@ import java.util.Map;
 /**
  * Created by kvanwijngaarden on 27/06/2017.
  */
+
+/*
+    Host: 83.87.212.114
+    User: root
+    Pass: mynewpassword
+    Port: 3306
+    Database: mysqltarget
+
+ */
+
 public class Mysql implements Ijdbc {
 
     public Connection createConnection(String DB_URL, String USER, String PASS) throws SQLException {
