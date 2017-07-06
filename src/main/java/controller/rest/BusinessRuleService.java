9436d87816a3b98ebba56f1e701760be3fab80e5
kvanwijngaarden <kjeldvanwijngaarden@gmail.com>
2017-07-06 14:03:49 +0200

    Enable, disable en delete van oracle db.. mysql moet nog komen. De DOA voor alles ook

--- src/main/java/controller/rest/BusinessRuleService.java
+++ src/main/java/controller/rest/BusinessRuleService.java
@@ -16,7 +16,23 @@ import javax.print.attribute.standard.Media;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.Map;
+
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Created by JariPC on 3-7-2017.
@@ -96,6 +112,62 @@ public class BusinessRuleService {
         result = a.CollectCollumns(map);
 
         return Response.ok(result).header("Access-Control-Allow-Origin","*").build();
+    }
+
+    @Path("/test")
+    @GET
+    @Produces({MediaType.APPLICATION_XML})
+    public Response test() throws SQLException{
+        DaoService doa = new DaoService();
+
+
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+            // root elements
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("company");
+            doc.appendChild(rootElement);
+
+            // staff elements
+            Element staff = doc.createElement("Staff");
+            rootElement.appendChild(staff);
+
+            // set attribute to staff element
+            Attr attr = doc.createAttribute("id");
+            attr.setValue("1");
+            staff.setAttributeNode(attr);
+
+            // firstname elements
+            Element firstname = doc.createElement("firstname");
+            firstname.appendChild(doc.createTextNode("yong"));
+            staff.appendChild(firstname);
+
+            // lastname elements
+            Element lastname = doc.createElement("lastname");
+            lastname.appendChild(doc.createTextNode("mook kim"));
+            staff.appendChild(lastname);
+
+            // nickname elements
+            Element nickname = doc.createElement("nickname");
+            nickname.appendChild(doc.createTextNode("mkyong"));
+            staff.appendChild(nickname);
+
+            // salary elements
+            Element salary = doc.createElement("salary");
+            salary.appendChild(doc.createTextNode("100000"));
+            staff.appendChild(salary);
+
+
+            return Response.ok(doc).header("Access-Control-Allow-Origin","*").build();
+
+        }catch (ParserConfigurationException e){
+            e.printStackTrace();
+        }
+
+
+        return Response.ok("Error").header("Access-Control-Allow-Origin","*").build();
     }
 
 
