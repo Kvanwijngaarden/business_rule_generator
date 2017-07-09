8c49c5b32c12bf45548937f344b5fe1c50f865b8
Martijn <martijn.dull@student.hu.nl>
2017-07-09 14:09:44 +0200

    validation error fix test

--- src/main/java/controller/validation/AttributeRangeRule.java
+++ src/main/java/controller/validation/AttributeRangeRule.java
@@ -10,8 +10,8 @@ public class AttributeRangeRule implements IValidate {
     public boolean Validate(Map<String, String> Brule) {
         ValidationFunctions vf = new ValidationFunctions();
 
-        String[] fieldstrigger = "GLANGUAGE_LANG_ID|TARGET_TABLE|TARGET_COLUMN|GRULETYPE_RULETYPE_ID|TRIGGER_EVENT|TRIGGER_ON|OPERATOR|VALUE|VALUE2".split("\\|");
-        String[] fieldscontraint = "GLANGUAGE_LANG_ID|TARGET_TABLE|TARGET_COLUMN|GRULETYPE_RULETYPE_ID|OPERATOR|VALUE|VALUE2".split("\\|");
+        String[] fieldstrigger = "TARGET_TABLE|TARGET_COLUMN|GRULETYPE_RULETYPE_ID|TRIGGER_EVENT|TRIGGER_ON|OPERATOR|VALUE|VALUE2".split("\\|");
+        String[] fieldscontraint = "TARGET_TABLE|TARGET_COLUMN|GRULETYPE_RULETYPE_ID|OPERATOR|VALUE|VALUE2".split("\\|");
 
         if (Brule.get("TRIGGER_EVENT") != null || Brule.get("TRIGGER_ON") != null) {
             if (!vf.EmptyCheck(Brule, fieldstrigger)){
